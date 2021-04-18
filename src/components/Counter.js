787be1414bf9b217fbea366e2e97807c5176b873
Mehul Azad <mazad@Mehuls-MBP.lan>
2021-04-18 13:42:36 -1000

    Pass callback function to setState() to log the new count value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,9 +13,8 @@ class Counter extends Component {
     incrementCount() {
         this.setState({
             count : this.state.count + 1
-        })
+        }, () => {console.log(this.state.count)})
     }
-    
 
     render() {
         return (
