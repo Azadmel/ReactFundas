f0818e63ee430d3ecc5cd66c8ab6f04ac23478f3
Mehul Azad <mazad@Mehuls-MBP.lan>
2021-04-18 11:09:11 -1000

    Add new class component called Message

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet'
 import Welcome from './components/Welcome'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Mehul" heroName="Marvelous"/>
+      {/* <Greet name="Mehul" heroName="Marvelous"/>
       <Greet name="Chris" heroName="Cutie"/>
-      <Welcome name="Mehul" heroName="Mayflower"></Welcome>
+      <Welcome name="Mehul" heroName="Mayflower"></Welcome> */}
+      <Message></Message>
     </div>
   );
 }
