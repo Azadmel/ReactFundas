2efc6cdf152faf871bee8b3eb3ac8a68c70ab28f
Mehul Azad <mazad@Mehuls-MBP.lan>
2021-04-19 10:47:51 -1000

    Simplify return

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 function NameList() {
     const names = ['Mehul', 'Chi', 'Angel']
+    const jsxNameList = names.map(name => <h2>{name}</h2>)
 
     return (
         <div>
-            {
-                // inside curly braces because this javascrip exp needs to be evaluated
-                // the map method transforms each element of names array to jsx.
-                names.map(name => <h2>{name}</h2>)
-            }       
+            {jsxNameList}      
         </div>
     )
 }
