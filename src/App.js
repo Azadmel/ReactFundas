d0ad4bfa4bcfa0ffa6fdb03d09b4d75f393bc14c
Mehul Azad <mazad@Mehuls-MBP.lan>
2021-04-18 14:13:38 -1000

    On click event handling in a functional component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import {Greet} from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Mehul" heroName="Marvelous"/>
+      {/* <Greet name="Mehul" heroName="Marvelous"/>
       <Welcome name="Mehul" heroName="Mayflower"></Welcome>
 
       <Message></Message>
-      <Counter></Counter>
+      <Counter></Counter> */}
+      <FunctionClick/>
     </div>
   );
 }
