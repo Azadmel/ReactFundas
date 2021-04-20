282c5cdc2f6eda7a808dcd9dab0bfd43e74c74fb
Mehul Azad <mazad@Mehuls-MBP.lan>
2021-04-20 09:36:27 -1000

    Make Form a controlled component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            username : ''
+        }
+    }
+    
+    usernameChangeHandler = (event) => {
+        this.setState({
+            username : event.target.value
+        })
+    }
+
     render() {
         return (
             <form>
                 <label>username</label>
-                <input type='text'></input>
+                <input type='text' value={this.state.username} onChange={this.usernameChangeHandler}></input>
             </form>
         )
     }
